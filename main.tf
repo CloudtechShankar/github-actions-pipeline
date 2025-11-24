872ce7e860e181e4d0a9ced573dc51ddeaaf0979
CloudtechShankar <shankaryalagandula2004@gmail.com>
2025-11-24 12:43:33 +0530

    Refactor AWS resources in main.tf
    Removed unused VPC and subnet resources, added new S3 bucket.

--- main.tf
+++ main.tf
@@ -1,17 +1,21 @@
-resource "aws_vpc" "name" {
-    cidr_block = "10.0.0.0/16"
-    tags = {
-      Name = "test_vpc"
-    }
-  
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-0ecb62995f68bb549"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.public.id
 }
 
 
-resource "aws_subnet" "name2" {
-    vpc_id = aws_vpc.name.id
-    cidr_block = "10.0.0.0/24"
-    tags = {
-      Name = "mysubnet"
-    }
+
+resource "aws_s3_bucket" "name" {
+    bucket = "yalagandula-shankar-netha-95"
   
+
 }
